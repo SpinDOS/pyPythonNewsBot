47ad6ccd14df368f5ab46abd9a1b97d3bf87be3a
Alexander Platonov <alexanderplat757@gmail.com>
2017-03-08 00:54:12 +0300

    Add update timeout

--- unique_news_picker.py
+++ unique_news_picker.py
@@ -4,13 +4,15 @@ import time
 
 
 class UniqueNewsPicker(object):
+    _UPDATE_TIMEOUT = datetime.timedelta(minutes=30)
 
     def __init__(self, db_manager, update_manager):
         self._db_manager = db_manager
         self._update_manager = update_manager
         self._actual_news = []
-        self._db_and_list_sync_blocks = [False, False]
         self._users_sync_blocks = []
+        self._last_update_from_db = datetime.datetime(1, 1, 1)
+        self._last_old_news_removal = datetime.datetime(1, 1, 1)
 
     def get_random_actual_news_for_user(self, user_id):
         while user_id in self._users_sync_blocks:
@@ -35,32 +37,28 @@ class UniqueNewsPicker(object):
         if actual_news_for_user:
             return actual_news_for_user
         self._update_manager.update_news_in_db()
-        self._update_news_in_lists_from_db(day_ago)
+        self._update_actual_news_list_from_db(day_ago)
         return self.get_stored_actual_news_for_user(user_id)
 
     def _remove_old_news_from_list(self, date_of_oldest_news):
-        if self._db_and_list_sync_blocks[1]:
-            while self._db_and_list_sync_blocks[1]:
-                time.sleep(1)
+        now = datetime.datetime.now()
+        if self._last_old_news_removal + self._UPDATE_TIMEOUT > now:
             return
-        self._db_and_list_sync_blocks[1] = True
+        self._last_old_news_removal = now
         for news in self._actual_news:
             if news['datetime'] < date_of_oldest_news:
                 self._actual_news.remove(news)
-        self._db_and_list_sync_blocks[1] = False
 
-    def _update_news_in_lists_from_db(self, oldest_news_date):
-        if self._db_and_list_sync_blocks[0]:
-            while self._db_and_list_sync_blocks[0]:
-                time.sleep(1)
+    def _update_actual_news_list_from_db(self, oldest_news_date):
+        now = datetime.datetime.now()
+        if self._last_update_from_db + self._UPDATE_TIMEOUT > now:
             return
-        self._db_and_list_sync_blocks[0] = True
+        self._last_update_from_db = now
         for news in self._db_manager.load_db_from_file() or []:
             if news['datetime'] >= oldest_news_date and \
                     not self._is_actual_news_contains_news(news):
                 news['people'] = []
                 self._actual_news.append(news)
-        self._db_and_list_sync_blocks[0] = False
 
     def _is_actual_news_contains_news(self, news_to_check):
         for news in self._actual_news:
